661bf6c5bddf2402184e7dd2f856bc18f92ffea7
Guru Pawar <g.r.pawar000@gmail.com>
2020-08-08 18:16:25 +0530

    initial commit

---
+++ dist/app.js
@@ -0,0 +1,29 @@
+let linkWrapper = document.querySelector(".link_wrapper");
+let URL = "./links.json";
+function myLinks() {
+  fetch(URL)
+    .then((res) => {
+      if (res.ok) {
+        return res.json();
+      } else {
+        return new Error("Something wents wrong");
+      }
+    })
+    .then((data) => {
+      data.forEach((element) => {
+        let links = document.createElement("div");
+        links.setAttribute("class", "links");
+        let a = document.createElement("a");
+        a.textContent = element.caption;
+
+        links.addEventListener("click", function () {
+          location.href = element.url;
+        });
+
+        linkWrapper.appendChild(links);
+        links.append(a);
+      });
+    });
+}
+
+myLinks();
